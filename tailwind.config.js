20223863ba895fbf04168953a9e12cf7930ccd36
Rediet Woudma <139815109+Rediet-W@users.noreply.github.com>
2025-04-09 00:39:35 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
    "./src/**/*.{js,ts,jsx,tsx}", 
     "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
