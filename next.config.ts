38ad8fb5907282eda25364155afc9012ebf17780
Rediet Woudma <139815109+Rediet-W@users.noreply.github.com>
2025-04-09 10:40:39 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   /* config options here */
-  reactStrictMode: true,
-   swcMinify: true,
-   compiler: {
-     // Enables the styled-components SWC transform
-     styledComponents: true
-   },
-   output: 'standalone'
- };
+  
 };
 
 export default nextConfig;
